5d45a81f8d72122a073845abd0e8759dd9467ff3
Naoto Eguchi <naoto725@gmail.com>
2018-06-03 00:50:14 +0000

    bootstrap対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: "tops#top"
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
